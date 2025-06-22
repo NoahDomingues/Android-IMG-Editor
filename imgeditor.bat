8552de410321029d6e6055ce7aaf962502c8eb16
Noah Domingues <noahsdom@gmail.com>
2025-06-22 20:34:14 +0100

    Update imgeditor.bat

--- imgeditor.bat
+++ imgeditor.bat
@@ -1,5 +1,50 @@
 ECHO OFF
 CLS
+
+setlocal
+title Android IMG Editor
+
+color 1F
+echo(
+echo ==================== Android IMG Editor - v1.0 ====================
+echo ======================== By Noah Domingues ========================
+echo(
+echo --------------- Based on JordanEJ's IMG Editor Tool ---------------
+echo(
+echo ---------- Discord Server: https://discord.gg/3zbfaTNN7V ----------
+echo(
+
+REM -------------------------------
+REM Step 1: Extract bin.zip into the "bin" folder
+REM -------------------------------
+if exist bin.zip (
+    title Android IMG Editor - Extracting...
+    echo Extracting files...
+    powershell -Command "Expand-Archive -Force 'bin.zip' -DestinationPath 'bin'"
+    if %errorlevel% neq 0 (
+        title Android IMG Editor - Extraction Failed
+        echo ERROR: Failed to extract bin.zip. Join our Discord server for assistance: https://discord.gg/3zbfaTNN7V
+        pause
+        exit /b %errorlevel%
+    )
+    attrib +h "bin"
+    title Android IMG Editor - Extraction Complete
+    echo Extraction complete.
+) else (
+    title Android IMG Editor - Extraction Failed
+    echo ERROR: bin.zip not found. Join our Discord server for assistance: https://discord.gg/3zbfaTNN7V
+    pause
+    exit /b 1
+)
+
+REM -------------------------------
+REM Step 2: Delete bin.zip after extraction
+REM -------------------------------
+del bin.zip /f
+title Android IMG Editor
+cls
+
+
 :MENU
 cls
 ECHO.
@@ -11,9 +56,11 @@ set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| |
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
 ECHO.
-ECHO 1 - Unpack
-ECHO 2 - Repack
-ECHO 3 - Clear Unpacked Files
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
+ECHO.
+ECHO 1 - Unpack .img
+ECHO 2 - Repack .img
+ECHO 3 - Clear unpacked files
 ECHO 4 - EXIT
 ECHO.
 SET /P M=Option:
@@ -32,6 +79,8 @@ set /P "=-| || |\/| | |  _  |  _| | | | | |  | || | | | |_) |   | || | | | | | |
 set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| | |___ " < NUL & echo/
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
+ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
 ECHO.
 ECHO 1 - Unpack system.img
 ECHO 2 - Unpack vendor.img
@@ -56,6 +105,8 @@ set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| |
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
 ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
+ECHO.
 ECHO 1 - Repack system.img
 ECHO 2 - Repack vendor.img
 ECHO 3 - Repack system.img and vendor.img
@@ -78,6 +129,8 @@ set /P "=-| || |\/| | |  _  |  _| | | | | |  | || | | | |_) |   | || | | | | | |
 set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| | |___ " < NUL & echo/
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
+ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
 ECHO.
 if exist bin\tmp rd /s /q bin\tmp
 if exist editor rd /s /q editor
@@ -113,6 +166,8 @@ set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| |
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
 ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
+ECHO.
 if exist bin\tmp rd /s /q bin\tmp
 if exist editor rd /s /q editor
 if not exist bin\tmp mkdir bin\tmp
@@ -145,6 +200,8 @@ set /P "=-| || |\/| | |  _  |  _| | | | | |  | || | | | |_) |   | || | | | | | |
 set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| | |___ " < NUL & echo/
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
+ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
 ECHO.
 if exist bin\tmp rd /s /q bin\tmp
 if exist editor rd /s /q editor
@@ -187,21 +244,36 @@ set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| |
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
 ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
+ECHO.
+
 if not exist editor\system (
-echo - Please unpack a system.img first !!!
-pause
-GOTO MENU
+    echo - Please unpack a system.img first!
+    pause
+    GOTO MENU
 )
 
 if exist bin\tmp rd /s /q bin\tmp
 mkdir bin\tmp
 
-set /p systemsize=<"editor\system_size.txt"
-echo - Repack system
-bin\windows\make_ext4fs -L system -T -1 -S editor\system_file_contexts -C editor\system_fs_config -l %systemsize% -a system new_system.img editor\system\ >nul
+:: Check if system.img already exists and rename it
+if exist system.img (
+    echo - system.img already exists. Renaming to original_system.img ...
+    ren system.img original_system.img
 )
 
-if exist new_system.img echo - system.img has been repacked
+set /p systemsize=<"editor\system_size.txt"
+echo - Repack system
+:: Check if system.img exists and rename it
+if exist system.img (
+    echo Renaming existing system.img to original_system.img...
+    rename system.img original_system.img
+)
+
+:: Repack the new system image as system.img
+bin\windows\make_ext4fs -L system -T -1 -S editor\system_file_contexts -C editor\system_fs_config -l %systemsize% -a system system.img editor\system\ >nul
+
+if exist system.img echo - system.img has been repacked
 
 pause
 GOTO MENU
@@ -217,8 +289,10 @@ set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| |
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
 ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
+ECHO.
 if not exist editor\vendor (
-echo - Please unpack vendor.img first !!!
+echo - Please unpack vendor.img first!
 pause
 GOTO MENU
 )
@@ -245,8 +319,10 @@ set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| |
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
 ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
+ECHO.
 if not exist editor\system (
-echo - Please unpack system.img first !!!
+echo - Please unpack system.img first!
 pause
 GOTO MENU
 )
@@ -256,7 +332,14 @@ mkdir bin\tmp
 
 set /p systemsize=<"editor\system_size.txt"
 echo - Repacking system.img
-bin\windows\make_ext4fs -L system -T -1 -S editor\system_file_contexts -C editor\system_fs_config -l %systemsize% -a system new_system.img editor\system\ >nul
+:: Check if system.img exists and rename it
+if exist system.img (
+    echo Renaming existing system.img to original_system.img...
+    rename system.img original_system.img
+)
+
+:: Repack the new system image as system.img
+bin\windows\make_ext4fs -L system -T -1 -S editor\system_file_contexts -C editor\system_fs_config -l %systemsize% -a system system.img editor\system\ >nul
 )
 
 if exist new_system.img echo - system.img has been repacked
@@ -285,6 +368,8 @@ set /P "=-| || |\/| | |  _  |  _| | | | | |  | || | | | |_) |   | || | | | | | |
 set /P "=-| || |  | | |_| | | |___| |_| | |  | || |_| |  _ <    | || |_| | |_| | |___ " < NUL & echo/
 set /P "=|___|_|  |_|\____| |_____|____/___| |_| \___/|_| \_\   |_| \___/ \___/|_____|" < NUL & echo/
 set /P "=/////////////////////////////////////////////////////////////////////////////" < NUL & echo/
+ECHO.
+ECHO DISCORD SERVER: https://discord.gg/3zbfaTNN7V
 ECHO.
 echo - Removing Files
 rmdir /s /Q editor
